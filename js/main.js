2f12f1a6f4035cb297a307e662b924c19b332981
Наталья <0207natalya@gmail.com>
2022-06-08 11:14:04 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(".bike__slider, .slider__items").slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    fade: true,
+    speed: 800,
+    cssEase: "linear",
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
